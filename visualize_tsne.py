db2a71105f4620f1f012eb6d7ceb8dacd341a584
hyojiny1 <yhjin210@gmail.com>
2018-10-03 16:19:58 +0900

    tsne

--- visualize_tsne.py
+++ visualize_tsne.py
@@ -10,23 +10,23 @@ import cv2
 from faster_rcnn.utils.timer import Timer
 from faster_rcnn.fast_rcnn.config import cfg, cfg_from_file
 
-def draw_graph(name):
+def draw_graph(fig, name):
     plt.title('Embedded Anchors')
-    fig = plt.gcf()
     fig.savefig(name+'.jpg')
 # hyper-parameters
 # ------------
-id_limit = 20
+
 imdb_name = 'CaltechPedestrians_test_triplet'
 
 cfg_file = 'experiments/cfgs/faster_rcnn_end2end.yml'
 model_dir = 'data/pretrained_model/'
 output_dir = 'models/saved_model3'
-pre_model_name = 'CaltechPedestrians_train_triplet_1_vgg16_cls_0.7_b3_relu.h5'
+pre_model_name = 'CaltechPedestrians_train_triplet_1_vgg16_cls_0.7_b3.h5'
 pretrained_model = model_dir + pre_model_name
 
 _DEBUG = False
 BG_SHOW = True
+id_limit = 20 if BG_SHOW else 50
 # load config
 cfg_from_file(cfg_file)
 # load data
@@ -51,18 +51,19 @@ t = Timer()
 t.tic()
 for i in range(test_num):
     image = cv2.imread(roidb[i]['image'])
-    boxes = roidb[i]['boxes'].astype(np.float32)
+    gt_boxes = roidb[i]['boxes'].astype(np.float32)
     relu = True if 'relu' in name_blocks else False
-    features.append(detector.extract_feature_vector(image, blob, boxes, relu=relu).data.cpu().numpy())
+    features.append(detector.extract_feature_vector(image, blob, gt_boxes, relu=relu).data.cpu().numpy())
     ids.append(roidb[i]['ids'][0])
     if BG_SHOW:
-        bg_features.append(detector.extract_background_features(image, blob, boxes, relu=relu).data.cpu().numpy())
+        bg_features.append(detector.extract_background_features(image, blob, gt_boxes, relu=relu).data.cpu().numpy())
     if len(set(ids)) > id_limit:
         break
 
 print('{:3.2f}s feature extraction finished !'.format(t.toc(average=False)))
 features = np.asarray(features, dtype=np.float32)
-model = TSNE(learning_rate=10)
+learning_rate = 100
+model = TSNE(learning_rate=learning_rate)
 labels = np.array(ids) % 4
 fig, ax = plt.subplots()
 font = {'family': 'serif',
@@ -78,13 +79,16 @@ ax.scatter(xs, ys, marker='+', c=labels)
 for i, id in enumerate(ids):
     ax.text(xs[i], ys[i], str(id), fontdict=font)
 # Negative(Background) Anchors
-neg_data = model.fit_transform(bg_features)
-xs = neg_data[:, 0]
-ys = neg_data[:, 1]
-ax.scatter(xs, ys, marker='x', c='r')
-font['color'] = 'black'
-for i, id in enumerate(ids):
-    ax.text(xs[i], ys[i], 'bg_' + str(id), fontdict=font)
+if BG_SHOW:
+    neg_data = model.fit_transform(bg_features)
+    xs = neg_data[:, 0]
+    ys = neg_data[:, 1]
+    ax.scatter(xs, ys, marker='x', c='r')
+    font['color'] = 'black'
+    for i, id in enumerate(ids):
+        ax.text(xs[i], ys[i], 'bg_' + str(id), fontdict=font)
 
 plt.show()
-draw_graph(pre_model_name)
+pre_model_name = pre_model_name.replace('.h5', '_'+str(learning_rate))
+pre_model_name += '_bg' if BG_SHOW else ''
+draw_graph(fig, pre_model_name)
